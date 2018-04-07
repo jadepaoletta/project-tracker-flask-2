e17fa50a404b6923107be2296782b3c91abff196
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-07 12:54:34 -0700

    student form to add new student

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,6 +1,6 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 
 import hackbright
 
@@ -15,7 +15,7 @@ def get_student():
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return render_template('student_info.html', github=github, first=first, 
+    return render_template('student_info.html', github=github, first=first,
                             last=last)
 
 @app.route("/student-search")
@@ -24,6 +24,26 @@ def get_student_form():
 
     return render_template("student_search.html")
 
+@app.route("/student-add", methods=['GET', 'POST'])
+def student_add():
+    """Add a student."""
+    # import pdb; pdb.set_trace()
+    # print request.form
+
+    if request.method == 'POST':
+        first = request.form.get("first")
+        last = request.form.get("last")
+        github = request.form.get("github")
+
+        hackbright.make_new_student(first, last, github)
+
+        flash("Successfully added student {} {}".format(first, last))
+        return render_template("student_add.html", link=True, github=github)
+
+
+    return render_template("student_add.html", link=False)
+
 if __name__ == "__main__":
+    app.secret_key = 'abc'
     hackbright.connect_to_db(app)
     app.run(debug=True)
